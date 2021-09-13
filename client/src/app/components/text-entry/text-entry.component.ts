7f76ba3248be81420bdd721e79084ffe2549b2f9
KevinClapperton <43681350+KevinClapperton@users.noreply.github.com>
2021-09-13 11:59:10 -0400

    KC [feature/boiteCommunication] Basic UI for chat and generating components

---
+++ client/src/app/components/text-entry/text-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-entry',
+  templateUrl: './text-entry.component.html',
+  styleUrls: ['./text-entry.component.scss']
+})
+export class TextEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
