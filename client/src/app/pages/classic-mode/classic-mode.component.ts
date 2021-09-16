8ac8f127d2a8640e755ec2eeb608325f4ba9c0b5
dieyba <77936435+dieyba@users.noreply.github.com>
2021-09-16 10:40:11 -0400

    [DD/EF] nettoyage de code

--- client/src/app/pages/classic-mode/classic-mode.component.ts
+++ client/src/app/pages/classic-mode/classic-mode.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-classic-mode',
-  templateUrl: './classic-mode.component.html',
-  styleUrls: ['./classic-mode.component.scss']
+    selector: 'app-classic-mode',
+    templateUrl: './classic-mode.component.html',
+    styleUrls: ['./classic-mode.component.scss'],
 })
-export class ClassicModeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ClassicModeComponent {}
--- client/src/app/pages/starting-page/starting-page.component.ts
+++ client/src/app/pages/starting-page/starting-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-starting-page',
-  templateUrl: './starting-page.component.html',
-  styleUrls: ['./starting-page.component.scss']
+    selector: 'app-starting-page',
+    templateUrl: './starting-page.component.html',
+    styleUrls: ['./starting-page.component.scss'],
 })
-export class StartingPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class StartingPageComponent {}
