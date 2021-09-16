296d5903cccacbe4784bd3161fd5e2c0e9a0ba93
sarabrochu <sara.brochu@polymtl.ca>
2021-09-16 16:13:31 -0400

    afficher messages colores selon l'auteur

--- client/src/app/components/chat-display/chat-display.component.ts
+++ client/src/app/components/chat-display/chat-display.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+const SYSTEM = "system";
+const ADVERSARY = "adversary"
+const PLAYER = "player"
+
+
 @Component({
   selector: 'app-chat-display',
   templateUrl: './chat-display.component.html',
   styleUrls: ['./chat-display.component.scss']
 })
-export class ChatDisplayComponent implements OnInit {
 
+export class ChatDisplayComponent implements OnInit {
+  entries: ChatEntry[] = [];
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
+  addPlayerEntry(isAdversary: boolean, playerMessage: string):void {
+    this.entries.push({author: (isAdversary)? ADVERSARY: PLAYER, message:"player message" });
+  }
+  
+  addError(errorMessage: string):void {
+    this.entries.push({author:SYSTEM, message:"error message" });
+  }
+
+
+}
+
+ interface ChatEntry {
+  author: string
+  message : string
 }
