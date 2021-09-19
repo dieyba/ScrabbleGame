3652f6ee2e2560de760b52fe0a491e790b8ddf1c
dieyba <77936435+dieyba@users.noreply.github.com>
2021-09-19 12:17:34 -0400

    [DD/EF] style et tests ToDo

--- client/src/app/components/form/form.component.ts
+++ client/src/app/components/form/form.component.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'app-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-    constructor() {}
+    name: string;
+    timer: number;
+    bonus: boolean;
+    dictionnary: string;
+    debutantNameList: string[];
+    expertNameList: string[];
+    selectedPlayer: string;
+    random: number;
+
+    constructor() {
+        this.name = '';
+        this.timer = 60;
+        this.bonus = false;
+        this.dictionnary = 'Francais(defaut)';
+        this.debutantNameList = ['erika', 'etienne', 'sara'];
+        this.expertNameList = ['dieyna', 'kevin', 'arianne'];
+    }
+    openForms(): void {
+        document.getElementById('form')!.style.display = 'block';
+    }
+    openList(): void {
+        document.getElementById('opponents')!.style.display = 'block';
+    }
+    onSelect(opponent: string): void {
+        document.getElementById('opponents')!.style.display = 'block';
+        this.selectedPlayer = opponent;
+    }
+    randomPlayer(list: string[]): void {
+        document.getElementById('opponents')!.style.display = 'block';
+        let randomFloat = Math.random() * (list.length + 1);
+        this.random = randomFloat;
+        this.selectedPlayer = list[Math.floor(randomFloat)];
+    }
+
+    /**  ANCIENNE VERSION
+    constructor() {
+        this.error = false;
+        this.errorMessage = '';
+        this.form = new Player();
+    }
+    form: Player;
+    error: boolean;
+    errorMessage: String;
 
     openForms(): void {
         document.getElementById('form')!.style.display = 'block';
     }
+    openList(): void {
+        document.getElementById('opponents')!.style.display = 'block';
+    }
+    confirmName(): void {
+        if (this.form.getName().length < 4 || this.form.getName().length > 12) {
+            this.form.nameValid = false;
+            this.error = true;
+            this.errorMessage = 'Ce nom contient des caractères invalides!';
+        } else {
+        }
+    }
+    validateName(event: KeyboardEvent): void {
+        if (event.key === 'Enter') {
+            this.confirmName();
+        }
+    }*/
 }
