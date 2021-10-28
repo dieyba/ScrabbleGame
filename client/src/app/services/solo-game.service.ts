0204b595c13759fbf53b24866f798430477a7d64
Kevin Clapperton <kevin.clapperton@polymtl.ca>
2021-10-27 23:05:36 -0400

    KC [feature/commandPlacerLettres] Migrating placing logic to its service

--- client/src/app/services/solo-game.service.ts
+++ client/src/app/services/solo-game.service.ts
@@ -9,11 +9,11 @@ import { ScrabbleBoard } from '@app/classes/scrabble-board';
 import { ScrabbleLetter } from '@app/classes/scrabble-letter';
 import { ScrabbleWord } from '@app/classes/scrabble-word';
 import { Axis } from '@app/classes/utilities';
-import { Vec2 } from '@app/classes/vec2';
 import { VirtualPlayer } from '@app/classes/virtual-player';
 import { LetterStock } from '@app/services/letter-stock.service';
 import { ChatDisplayService } from './chat-display.service';
 import { GridService } from './grid.service';
+import { PlaceService } from './place.service';
 import { RackService } from './rack.service';
 import { ValidationService, WAIT_TIME } from './validation.service';
 import { WordBuilderService } from './word-builder.service';
@@ -45,6 +45,7 @@ export class SoloGameService {
         private chatDisplayService: ChatDisplayService,
         private validationService: ValidationService,
         private wordBuilder: WordBuilderService,
+        private placeService: PlaceService,
     ) {
         this.hasTurnsBeenPassed = [];
         this.turnPassed = false;
@@ -233,118 +234,41 @@ export class SoloGameService {
         return totalValue;
     }
     place(player: Player, placeParams: PlaceParams): ErrorType {
-        const tempCoord = new Vec2();
-        // Checking if its player's turn
-        if (!this.canPlaceWord(player, placeParams)) {
-            return ErrorType.SyntaxError;
-        }
-        // Removing all the letters from my "word" that are already on the board
-        let wordCopy = placeParams.word;
-        const letterOnBoard = this.gridService.scrabbleBoard.getStringFromCoord(
-            placeParams.position,
-            placeParams.word.length,
-            placeParams.orientation,
-        );
-        for (const letter of letterOnBoard) {
-            wordCopy = wordCopy.replace(letter.toLowerCase(), '');
-        }
-        // All letter are already placed
-        if (wordCopy === '') {
-            return ErrorType.SyntaxError;
-        }
-        // Checking if the rest of the letters are on the rack
-        for (const letter of player.letters) {
-            // If there is an star, removing a upper letter from "word" string
-            if (letter.character === '*') {
-                let upperLetter = '';
-                for (const wordLetter of wordCopy) {
-                    if (wordLetter === wordLetter.toUpperCase()) {
-                        upperLetter = wordLetter;
+        const errorResult = this.placeService.place(player, placeParams);
+
+        if (errorResult === ErrorType.NoError) {
+            // Generate all words created
+            let tempScrabbleWords: ScrabbleWord[];
+            if (placeParams.orientation === 'h') {
+                tempScrabbleWords = this.wordBuilder.buildWordsOnBoard(placeParams.word, placeParams.position, Axis.H);
+            } else {
+                tempScrabbleWords = this.wordBuilder.buildWordsOnBoard(placeParams.word, placeParams.position, Axis.V);
+            }
+            // Call validation method and end turn
+            setTimeout(() => {
+                if (!this.validationService.validateWords(tempScrabbleWords)) {
+                    // Retake letters
+                    const removedLetters = this.gridService.removeInvalidLetters(
+                        placeParams.position,
+                        placeParams.word.length,
+                        placeParams.orientation,
+                    );
+                    this.addRackLetters(removedLetters);
+                } else {
+                    // Score
+                    this.validationService.updatePlayerScore(tempScrabbleWords, player);
+                    // Take new letters
+                    const newLetters = this.stock.takeLettersFromStock(DEFAULT_LETTER_COUNT - player.letters.length);
+                    for (const letter of newLetters) {
+                        this.rackService.addLetter(letter);
+                        player.letters.push(letter);
                     }
                 }
-                wordCopy = wordCopy.replace(upperLetter, '');
-            } else {
-                wordCopy = wordCopy.replace(letter.character, '');
-            }
-        }
-        // There should be no letters left, else there is not enough letter on the rack to place de "word"
-        if (wordCopy !== '') {
-            return ErrorType.SyntaxError;
-        }
-        // Placing letters
-        tempCoord.clone(placeParams.position);
-        for (const letter of placeParams.word) {
-            if (!this.gridService.scrabbleBoard.squares[tempCoord.x][tempCoord.y].occupied) {
-                // Taking letter from player and placing it
-                this.placeLetter(player.letters, letter, tempCoord);
-            }
-            if (placeParams.orientation === 'h') {
-                tempCoord.x++;
-            } else {
-                tempCoord.y++;
-            }
-        }
-        // Generate all words created
-        let tempScrabbleWords: ScrabbleWord[];
-        if (placeParams.orientation === 'h') {
-            tempScrabbleWords = this.wordBuilder.buildWordsOnBoard(placeParams.word, placeParams.position, Axis.H);
-        } else {
-            tempScrabbleWords = this.wordBuilder.buildWordsOnBoard(placeParams.word, placeParams.position, Axis.V);
-        }
-        // Call validation method and end turn
-        setTimeout(() => {
-            if (!this.validationService.validateWords(tempScrabbleWords)) {
-                // Retake letters
-                const removedLetters = this.gridService.removeInvalidLetters(placeParams.position, placeParams.word.length, placeParams.orientation);
-                this.addRackLetters(removedLetters);
-            } else {
-                // Score
-                this.validationService.updatePlayerScore(tempScrabbleWords, player);
-                // Take new letters
-                const newLetters = this.stock.takeLettersFromStock(DEFAULT_LETTER_COUNT - player.letters.length);
-                for (const letter of newLetters) {
-                    this.rackService.addLetter(letter);
-                    player.letters.push(letter);
-                }
-            }
 
-            this.passTurn(player);
-        }, WAIT_TIME);
-        return ErrorType.NoError;
-    }
-    placeLetter(playerLetters: ScrabbleLetter[], letter: string, position: Vec2) {
-        // Position already occupied
-        if (this.gridService.scrabbleBoard.squares[position.x][position.y].occupied) {
-            return;
+                this.passTurn(player);
+            }, WAIT_TIME);
         }
-        // Making a temporary letter and checking if "*" is needed (for upper cases)
-        let tempLetter = letter;
-        if (tempLetter === tempLetter.toUpperCase()) {
-            tempLetter = '*';
-        }
-        for (let i = 0; i < playerLetters.length; i++) {
-            if (playerLetters[i].character === tempLetter) {
-                if (letter === letter.toUpperCase()) {
-                    playerLetters[i].character = letter;
-                }
-                playerLetters[i].tile = this.gridService.scrabbleBoard.squares[position.x][position.y];
-                this.gridService.drawLetter(playerLetters[i], position.x, position.y);
-                this.rackService.removeLetter(playerLetters[i]);
-                playerLetters.splice(i, 1);
-                break;
-            }
-        }
-    }
-    canPlaceWord(player: Player, placeParams: PlaceParams): boolean {
-        if (
-            !this.gridService.scrabbleBoard.isWordInsideBoard(placeParams.word, placeParams.position, placeParams.orientation) ||
-            (!this.gridService.scrabbleBoard.isWordPassingInCenter(placeParams.word, placeParams.position, placeParams.orientation) &&
-                !this.gridService.scrabbleBoard.isWordPartOfAnotherWord(placeParams.word, placeParams.position, placeParams.orientation) &&
-                !this.gridService.scrabbleBoard.isWordTouchingOtherWord(placeParams.word, placeParams.position, placeParams.orientation)) ||
-            !player.isActive
-        ) {
-            return false;
-        }
-        return true;
+
+        return errorResult;
     }
 }
