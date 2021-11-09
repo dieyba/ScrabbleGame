bab2de19365b100d499a6201ee146c0669837742
dieyba <77936435+dieyba@users.noreply.github.com>
2021-11-08 23:18:53 -0500

    DD/EF [dev] fixing environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'ec2-15-222-12-251.ca-central-1.compute.amazonaws.com/api', // TODO: Add remote server URL
-    socketUrl: 'ec2-15-222-12-251.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-15-222-12-251.ca-central-1.compute.amazonaws.com:3000/api', // TODO: Add remote server URL
+    socketUrl: 'http://ec2-15-222-12-251.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
 };
