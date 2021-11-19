72ee255d6de6432ff985d7353e26656420f05458
dieyba <77936435+dieyba@users.noreply.github.com>
2021-11-19 15:20:51 -0500

    DD [feature/meilleurs-scores] test bestScoresComponent done

--- client/src/app/components/best-scores/best-scores.component.spec.ts
+++ client/src/app/components/best-scores/best-scores.component.spec.ts
@@ -1,16 +1,33 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpStatusCode } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { BestScoresService } from '@app/services/best-scores.service';
+import { BASE_URL, BestScores, BestScoresService } from '@app/services/best-scores.service';
+import { of, throwError } from 'rxjs';
 import { BestScoresComponent } from './best-scores.component';
 
 describe('BestScoresComponent', () => {
     let component: BestScoresComponent;
     let fixture: ComponentFixture<BestScoresComponent>;
     let bestScoresServiceSpy: jasmine.SpyObj<BestScoresService>;
+    let defaultClassicBestScoresValue = [
+        {
+            playerName: 'Erika',
+            score: 1,
+        },
+        {
+            playerName: 'Sara',
+            score: 8,
+        },
+        {
+            playerName: 'Etienne',
+            score: 2,
+        },
+    ] as BestScores[];
     beforeEach(async () => {
         bestScoresServiceSpy = jasmine.createSpyObj('BestScoresService', ['getBestScores', 'handleErrorSnackBar']);
-        // bestScoresServiceSpy.getBestScores('') = new Observable<BestScores>();
+        bestScoresServiceSpy.getBestScores.and.callFake(() => {
+            return of(defaultClassicBestScoresValue)
+        })
         await TestBed.configureTestingModule({
             declarations: [BestScoresComponent],
             providers: [
@@ -38,5 +55,22 @@ describe('BestScoresComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('getClassicBestScores should handle error when getting', () => {
+        bestScoresServiceSpy.getBestScores.and.callFake(() => throwError({ status: HttpStatusCode.NotFound }));
+        component.getClassicBestScores(BASE_URL + '/classicMode');
+        expect(bestScoresServiceSpy.handleErrorSnackBar).toHaveBeenCalled();
+    });
+    it('getLog2990BestScores should handle error when getting', () => {
+        bestScoresServiceSpy.getBestScores.and.callFake(() => throwError({ status: HttpStatusCode.NotFound }));
+        component.getLog2990BestScores(BASE_URL + '/classicMode');
+        expect(bestScoresServiceSpy.handleErrorSnackBar).toHaveBeenCalled();
+    });
+
+    it('closeDialog shoud close dialog', () => {
+        const spy = spyOn(component['dialogRef'], 'close');
+        component.closeDialog();
+        expect(spy).toHaveBeenCalledTimes(1);
     });
 });
