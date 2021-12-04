5b4cb110e9e4092dfac0beff1cc28c0f9aa579a3
Sara Brochu <sara.brochu@polymtl.ca>
2021-12-03 21:57:34 -0500

    SB [fix/test] fix tests for mouse word placer and its companion

--- client/src/app/services/mouse-word-placer-companion.service/mouse-word-placer-companion.service.ts
+++ client/src/app/services/mouse-word-placer-companion.service/mouse-word-placer-companion.service.ts
@@ -3,8 +3,7 @@ import { ScrabbleBoard } from '@app/classes/scrabble-board/scrabble-board';
 import { SquareColor } from '@app/classes/square/square';
 import { Axis } from '@app/classes/utilities/utilities';
 import { Vec2 } from '@app/classes/vec2/vec2';
-import { BOARD_SIZE } from '@app/services/grid.service/grid.service';
-import { ABSOLUTE_BOARD_SIZE, ACTUAL_SQUARE_SIZE } from '@app/services/mouse-word-placer.service/mouse-word-placer.service';
+import { ABSOLUTE_BOARD_SIZE, ACTUAL_SQUARE_SIZE, BOARD_SIZE } from '@app/services/grid.service/grid.service';
 
 @Injectable({
     providedIn: 'root',
--- client/src/app/services/mouse-word-placer.service/mouse-word-placer.service.ts
+++ client/src/app/services/mouse-word-placer.service/mouse-word-placer.service.ts
@@ -7,11 +7,9 @@ import { Axis, isValidLetter, removeAccents } from '@app/classes/utilities/utili
 import { Vec2 } from '@app/classes/vec2/vec2';
 import { CommandInvokerService } from '@app/services/command-invoker.service/command-invoker.service';
 import { GameService } from '@app/services/game.service/game.service';
-import { BOARD_OFFSET, GridService, SQUARE_SIZE } from '@app/services/grid.service/grid.service';
+import { ABSOLUTE_BOARD_SIZE, ACTUAL_SQUARE_SIZE, BOARD_OFFSET, GridService, SQUARE_SIZE } from '@app/services/grid.service/grid.service';
 import { MouseWordPlacerCompanionService } from '@app/services/mouse-word-placer-companion.service/mouse-word-placer-companion.service';
 import { RackService, RACK_HEIGHT, RACK_WIDTH } from '@app/services/rack.service/rack.service';
-export const ABSOLUTE_BOARD_SIZE = 580;
-export const ACTUAL_SQUARE_SIZE = SQUARE_SIZE + 2;
 @Injectable({
     providedIn: 'root',
 })
